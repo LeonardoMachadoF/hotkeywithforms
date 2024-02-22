8981f08bde006c13545d81d3e4d53f1ddd643d47
Leonardo Machado <leonardomachado200112@gmail.com>
2024-02-22 15:33:54 -0300

    Adicionar arquivos de projeto.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.LightGray;
+            label1.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Image = Properties.Resources.Sem_título;
+            label1.Location = new Point(0, -1);
+            label1.MinimumSize = new Size(64, 64);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 64);
+            label1.TabIndex = 1;
+            label1.Text = "R";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Click += label1_Click_2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(64, 61);
+            Controls.Add(label1);
+            ForeColor = SystemColors.Info;
+            MaximizeBox = false;
+            MaximumSize = new Size(80, 100);
+            MinimizeBox = false;
+            MinimumSize = new Size(64, 64);
+            Name = "Form1";
+            Opacity = 0.7D;
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            TopMost = true;
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,102 @@
+using System.Runtime.InteropServices;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        async private void Form1_Load(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            [DllImport("user32.dll")]
+            static extern short GetAsyncKeyState(int vKey);
+            while(true)
+            {
+                if(GetAsyncKeyState(0x11) < 0 || GetAsyncKeyState(0x70) < 0) // Ctrl e F1
+                {
+                    StartTimer("17-5", TimeSpan.FromMinutes(1));
+                }
+
+                await Task.Delay(20);
+            }
+
+            void StartTimer(string message, TimeSpan duration)
+            {
+                if(!IsTimerRunning(message))
+                {
+                    Console.WriteLine($"Start {message}");
+                    TimerState timerState = new TimerState(message, DateTime.Now.Add(duration));
+                    timerState.Timer = new System.Threading.Timer(TimerCallback, timerState, 0, 1000);
+                    runningTimers.Add(timerState);
+                }
+            }
+
+            void TimerCallback(object state)
+            {
+                var timerState = (TimerState)state;
+                TimeSpan remainingTime = timerState.EndTime - DateTime.Now;
+                label1.BeginInvoke((MethodInvoker)delegate ()
+                {
+                    string secondsRemaining = remainingTime.Seconds.ToString("D2");
+                    label1.Text = secondsRemaining; 
+                });
+
+                if(remainingTime.TotalSeconds <= 0)
+                {
+                    Console.Beep();
+                    timerState.Timer.Change(Timeout.Infinite, Timeout.Infinite); // Parar o temporizador
+                    timerState.Timer.Dispose(); // Liberar recursos do temporizador
+                    runningTimers.Remove(timerState); // Remover o temporizador da lista de temporizadores em execução
+                    label1.BeginInvoke((MethodInvoker)delegate ()
+                    {
+                        label1.Text = "R";
+                    });
+                }
+            }
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+        class TimerState
+        {
+            public string Message { get; }
+            public DateTime EndTime { get; }
+            public System.Threading.Timer Timer { get; set; }
+
+            public TimerState(string message, DateTime endTime)
+            {
+                Message = message;
+                EndTime = endTime;
+            }
+        }
+
+        static List<TimerState> runningTimers = new List<TimerState>();
+        static bool IsTimerRunning(string message)
+        {
+            return runningTimers.Any(t => t.Message == message);
+        }
+
+        
+    }
+}
